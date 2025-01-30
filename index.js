b0fa9bdeb4a90c2a5c98e30d31885f7a243d43d9
komalkharche31 <komalkharche1989@gmail.com>
2025-01-30 17:25:06 +0530

    add git ignore file

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const fs = require('fs')
 const home = fs.readFileSync("./index.html")
 const about = fs.readFileSync("./about.html")
 const contact = fs.readFileSync("./contact.html")
+
 const PORT = "3000" 
 const hostname = "localhost"
+
 const server = http.createServer((req,res)=>{
     //console.log('Server is working');
     if(req.url === '/'){
